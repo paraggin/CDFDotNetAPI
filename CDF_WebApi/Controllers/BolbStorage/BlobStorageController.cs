7785c7adcd7be99bf3b92b7c24fdc604807da1d6
Parag <107133664+paraggin@users.noreply.github.com>
2024-04-03 11:44:48 +0530

    update

--- CDF_WebApi/Controllers/BolbStorage/BlobStorageController.cs
+++ CDF_WebApi/Controllers/BolbStorage/BlobStorageController.cs
@@ -7,8 +7,13 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using Azure.Storage.Blobs.Models;
 using Azure.Storage.Blobs.Specialized;
+using System.IO;
 
 using Constants = CDF_Services.Constants.Constants;
+using Azure.Storage;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Reflection.Metadata;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace CDF_WebApi.Controllers.BolbStorage
 {
@@ -39,7 +44,7 @@ namespace CDF_WebApi.Controllers.BolbStorage
             BlobClient blobClient = new BlobClient(_connectionString, container_name, prefix);
             using (var stream = new MemoryStream())
             {
-                await blobClient.DownloadToAsync(stream);
+               var temp= await blobClient.DownloadToAsync(stream);
                 stream.Position = 0;
                 var contentType = (await blobClient.GetPropertiesAsync()).Value.ContentType;
                 return File(stream.ToArray(), contentType, blobClient.Name);
@@ -53,8 +58,111 @@ namespace CDF_WebApi.Controllers.BolbStorage
            
            return await _BlobStorageService.ListBlobs(container_name, pageSize, pageNumber+1);
         }
-       
+
         [HttpGet]
+        [Route("getBLobSAS")]
+        public async Task<IActionResult> getBLobSAS(string BlobName)
+        {
+
+            const string AccountName = "blobpoc02";
+            const string AccountKey = "a8NjedNF5CBZ76krN/HDW5QXdDR8lapH1Pqh8flb1imX5MrsN3ZVv44BciaB9XTQK2mhtTHanvGK+AStqO7PFg==";
+            const string ContainerName = "container-poc";
+            const string ConnectionString = "DefaultEndpointsProtocol=https;AccountName=blobpoc02;AccountKey=a8NjedNF5CBZ76krN/HDW5QXdDR8lapH1Pqh8flb1imX5MrsN3ZVv44BciaB9XTQK2mhtTHanvGK+AStqO7PFg==;EndpointSuffix=core.windows.net";
+            using (StreamWriter writer = System.IO.File.AppendText("log.txt"))
+            {
+                BlobContainerClient blobContainerClient = new BlobContainerClient(ConnectionString,
+                ContainerName);
+
+                BlobClient blobClient = blobContainerClient.GetBlobClient(BlobName);
+
+                Azure.Storage.Sas.BlobSasBuilder blobSasBuilder = new Azure.Storage.Sas.BlobSasBuilder()
+                {
+                    BlobContainerName = ContainerName,
+                    BlobName =BlobName,
+                    ExpiresOn = DateTime.UtcNow.AddMinutes(15),
+                };
+                blobSasBuilder.SetPermissions(Azure.Storage.Sas.BlobSasPermissions.Read);
+                var sasToken = blobSasBuilder.ToSasQueryParameters(new
+                StorageSharedKeyCredential(AccountName, AccountKey)).ToString();
+                var sasURL = $"{blobClient.Uri.AbsoluteUri}?{sasToken}";
+                writer.WriteLine(sasURL);
+                return new JsonResult(new { blobSASUrl = sasURL });
+
+            }
+
+        }
+
+        [HttpGet]
+        [Route("ListBlobsv3")]
+        public async Task<IActionResult> ListBlobsv3(int pageSize, int pageNumber, string? searchText)
+        {
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=blobpoc02;AccountKey=a8NjedNF5CBZ76krN/HDW5QXdDR8lapH1Pqh8flb1imX5MrsN3ZVv44BciaB9XTQK2mhtTHanvGK+AStqO7PFg==;EndpointSuffix=core.windows.net";
+
+            var serviceClient = new BlobServiceClient(connectionString);
+
+            string containerName = "container-poc";
+            BlobContainerClient containerClient = serviceClient.GetBlobContainerClient(containerName);
+
+            int totalCount = 0;
+            await foreach (BlobItem blob in containerClient.GetBlobsAsync(traits: BlobTraits.None))
+            {
+                totalCount++;
+            }
+            List<TaggedBlobItem> blobs = new List<TaggedBlobItem>();
+
+            using (StreamWriter writer = System.IO.File.AppendText("log.txt"))
+            {
+
+                //   string query = $"\"Version\" = '{searchText}' | \"CreatedDate\" = '{searchText}' | \"ModifiedDate\" = '{searchText}'  | \"createdDate\" = '{searchText}' | \"modifiedDate\" = '{searchText}' | \"version\" = '{searchText}'";
+                string query = $"\"Version\" = '{searchText}' | \"CreatedDate\" = '{searchText}' | \"ModifiedDate\" = '{searchText}'  | \"createdDate\" = '{searchText}' | \"modifiedDate\" = '{searchText}' | \"version\" = '{searchText}'";
+
+                writer.WriteLine($"Query: {query}");
+
+                string continuationToken = null;
+                int skip = (pageNumber - 1) * pageSize;
+                await foreach (Page<TaggedBlobItem> page in containerClient.FindBlobsByTagsAsync(query)
+                    .AsPages(continuationToken, pageSize))
+                {
+                    if (skip >= page.Values.Count)
+                    {
+                        skip -= page.Values.Count;
+                        continuationToken = page.ContinuationToken;
+                        continue;
+                    }
+
+                    var items = page.Values.Skip(skip);
+
+                    foreach (TaggedBlobItem blobItem in items)
+                    {
+                        BlobClient blobClient = containerClient.GetBlobClient(blobItem.BlobName);
+
+                        BlobProperties blobProperties = await blobClient.GetPropertiesAsync();
+                        writer.WriteLine($"Properties: {blobProperties.ToString()}");
+
+                        writer.WriteLine($"Properties: {blobProperties.Metadata}");
+                        writer.WriteLine($"Properties: {blobProperties.TagCount}");
+
+                        /* writer.WriteLine($"  Content Type: {blobProperties.ContentType}");
+                         writer.WriteLine($"  Content Length: {blobProperties.ContentLength}");
+                         writer.WriteLine($"  Last Modified: {blobProperties.LastModified}");*/
+                        writer.WriteLine();
+
+
+                    }
+
+                    if (blobs.Count >= pageSize)
+                    {
+                        break;
+                    }
+
+                    skip = Math.Max(0, pageSize - blobs.Count);
+                    continuationToken = page.ContinuationToken;
+                }
+            }
+            return new JsonResult(new { TotalCount = totalCount, Blobs = blobs });
+        }
+
+       [HttpGet]
         [Route("ListBlobsv2")]
         public async Task<IActionResult> FilterBlobs( int pageSize, int pageNumber,string? searchText)
         {
@@ -75,16 +183,34 @@ namespace CDF_WebApi.Controllers.BolbStorage
             //modifiedDate
             //Version
 
-            List<string> searchConditions = new List<string>();
-            searchConditions.Add($"\"Version\" = '{searchText}'");
-            // searchConditions.Add($"\"CreatedDate\" = '{searchText}'");
-            //searchConditions.Add($"\"modifiedDate\" = '{searchText}'");
+            /*   List<string> searchConditions = new List<string>();
+               searchConditions.Add($"\"Version\" = '{searchText}'");          
+               string query = $"\"Version\" = '{searchText}' | \"CreatedDate\" = '{searchText}' | \"ModifiedDate\" = '{searchText}'  | \"createdDate\" = '{searchText}' | \"modifiedDate\" = '{searchText}' | \"version\" = '{searchText}'";
+   */
 
+            List<(string field, string value)> searchConditions = new List<(string field, string value)>();
+            searchConditions.Add(("Version", searchText));
+            searchConditions.Add(("CreatedDate", searchText));
+            searchConditions.Add(("ModifiedDate", searchText));
+            searchConditions.Add(("createdDate", searchText));
+            searchConditions.Add(("modifiedDate", searchText));
+            searchConditions.Add(("version", searchText));
 
-            // string query =  string.Join(" | ", searchConditions) ;
-            string query = $"\"Version\" = '{searchText}'";
+            searchConditions.Sort((x, y) => x.field.CompareTo(y.field));
 
-            List<TaggedBlobItem> blobs = new List<TaggedBlobItem>();
+            List<string> queryConditions = new List<string>();
+            foreach (var condition in searchConditions)
+            {
+                queryConditions.Add($"\"{condition.field}\" = '{condition.value}'");
+            }
+
+            string query = string.Join(" | ", queryConditions);
+            using (StreamWriter writer = System.IO.File.AppendText("log.txt"))
+            { 
+                writer.WriteLine(query);
+            }
+
+                List<TaggedBlobItem> blobs = new List<TaggedBlobItem>();
 
             string continuationToken = null;
             int skip = (pageNumber - 1) * pageSize;
