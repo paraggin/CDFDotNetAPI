ab950091e1cfd43b28f0f3d23936e763fda1136e
devanihiren <hirendevani.in@gmail.com>
2024-09-20 18:52:23 +0530

    update

--- CDF_WebApi/Controllers/BolbStorage/BlobKeyVaultController.cs
+++ CDF_WebApi/Controllers/BolbStorage/BlobKeyVaultController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using CDF_Services.IServices.IBlobStorageService;
+using CDF_Services.Services.BlobStorageService;
 
 namespace CDF_WebApi.Controllers.BolbStorage
 {
@@ -10,18 +11,30 @@ namespace CDF_WebApi.Controllers.BolbStorage
     public class BlobKeyVaultController : ControllerBase
     {
         private readonly IBlobKeyVaultService _blobKeyVaultService;
+        private readonly IBlobStorageService _blobStorageService;
 
-        public BlobKeyVaultController(IBlobKeyVaultService blobKeyVaultService) {
+        public BlobKeyVaultController(IBlobKeyVaultService blobKeyVaultService, IBlobStorageService blobStorageService)
+        {
             _blobKeyVaultService = blobKeyVaultService;
+            _blobStorageService = blobStorageService;
         }
 
 
         [HttpGet]
-        [Route("getBLobSasUrl")]
+        [Route("getBLobSasUrl_KeyVault")]
         public async Task<IActionResult> getBLobSasUrl(string storageAccountName, string containerName, string blobName)
         {
 
             return await _blobKeyVaultService.GenerateBlobSASUrl( storageAccountName,  containerName,  blobName);
+
+        }
+
+        [HttpGet]
+        [Route("getBLobSasUrl")]
+        public async Task<IActionResult> getBLobSasUrlDynamic(string storageAccountName, string containerName, string blobName)
+        {
+
+            return await _blobStorageService.getBlobSasUrl_Dynamic(storageAccountName, containerName, blobName);
 
         }
 
